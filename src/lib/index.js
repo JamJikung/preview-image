f1956d33454b4abe6c8e80e2722ff799812b6d50
manshowsir <jamjikung@icloud.com>
2019-11-12 15:02:49 +0800

    edit

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,4 +5,7 @@ const previewImage = {
     Vue.component(vuePreviewImage.name, vuePreviewImage) // vuePreviewImage.name 组件的name属性
   }
 }
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(previewImage)
+}
 export default previewImage // 导出
